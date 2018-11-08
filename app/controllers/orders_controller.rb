cb5f170213055e86f5e95035f9aadf1f90e5faff
Angela Larryant <angelarryant@gmail.com>
2018-11-08 22:22:27 +0000

    added enhanced order details

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,9 +2,6 @@ class OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
-    @line_item = LineItem.find_by order_id: params[:id]
-    @product = Product.find(@line_item.product_id)
-    p @product
   end
 
   def create
